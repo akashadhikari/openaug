7e084c431f88054e6d406a6813e68031cfd2afa3
akashadhikari <akash_532998@yahoo.com>
2017-01-26 09:43:29 +0545

    authenticated

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -10,15 +10,27 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Augmented</a>
+          <a class="navbar-brand" href="/">Augmented</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="{{ Request::is('/') ? "active" : "" }}"><a href="/">Home</a></li>
+            
             <li class="{{ Request::is('about') ? "active" : "" }}"><a href="/about">About</a></li>
             <li class="{{ Request::is('contact') ? "active" : "" }}"><a href="/contact">Contact</a></li>
+            
+          </ul>
+          <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search businesses and places">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+
+            @if(Auth::check())
+
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Actions <span class="caret"></span></a>
               <ul class="dropdown-menu">
@@ -31,16 +43,6 @@
                 <li><a href="#">One more separated link</a></li>
               </ul>
             </li>
-          </ul>
-          <form class="navbar-form navbar-left">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search businesses and places">
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-
-            @if(Auth::check())
 
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hi {{ Auth::user()->name }} <span class="caret"></span></a>
