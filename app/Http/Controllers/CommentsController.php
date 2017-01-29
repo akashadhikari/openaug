1962e6ee5a5917efcb9493fa56678a96ca548064
akashadhikari <akash_532998@yahoo.com>
2017-01-29 11:41:16 +0545

    comment-show-to-do

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use App\Post;
+use Session;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $post_id)
+    {
+        //validate the data
+        $this->validate($request, array(
+            'name'    => 'required|max:255',
+            'email'   => 'required|email|max:140',
+            'comment' => 'required|min:5|max:2000'
+            ));
+
+        $post = Post::find($post_id);
+
+        $comment = new Comment();
+        $comment->name = $request->name;
+        $comment->email = $request->email;
+        $comment->comment = $request->comment;
+        $comment->approved = true;
+        $comment->post()->associate($post);
+   
+        $comment->save();
+
+        Session::flash('success', 'Congratulations! Review has been published.');
+
+        //redirect to another page
+        return redirect()->route('augments.single', [$post->slug]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/augments/single.blade.php
+++ resources/views/augments/single.blade.php
@@ -1,6 +1,8 @@
 @extends('main')
 
-@section('title', "| $post->title")
+<?php $titleTag = htmlspecialchars($post->title); ?>
+
+@section('title', "| $titleTag")
 
 @section('content')
 
@@ -30,5 +32,51 @@
 		 </div>
 
 	</div>
+
+<!-- SHOWINFG THE COMMENTS NOT WORKING
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			@foreach($post->comments as $comment)
+				{{ $comment->comment }};
+			@endforeach
+		</div>
+	</div> -->
+
+	<div class="row">
+		<div class="comment-form" class="col-md-8 col-md-offset-2" style="margin-top: 50px;">
+			
+			{{ Form::open(['route' => ['comments.store', $post->id], 'method' => 'POST']) }}
+			<h3>Been Here? Add A Review</h3>
+ 			
+ 				<div class="row">
+
+ 					<div class="col-md-6">
+ 						{{ Form::label('name','Your Name') }}
+ 						{{ Form::text('name', null, ['class' => 'form-control']) }}
+ 					</div>
+
+ 					<div class="col-md-6">
+ 						{{ Form::label('email','Your Email') }}
+ 						{{ Form::text('email', null, ['class' => 'form-control']) }}
+ 					</div>
+
+ 					<div class="col-md-12">
+ 						{{ Form::label('comment','Review The Business') }}
+ 						{{ Form::textarea('comment', null, ['class' => 'form-control', 'rows'=> '5']) }}
+
+ 						{{ Form::submit('Submit Review', ['class' => 'btn btn-success btn-block', 'style' =>'margin-top: 15px;']) }}
+ 					</div>
+
+ 				</div>
+ 				
+
+ 				
+
+            {{ Form::close() }}
+
+		</div>
+	</div>
+
+
 
 @endsection
