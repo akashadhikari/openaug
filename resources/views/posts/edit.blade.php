e1721eb6d83c7203e24997e5697ab5e04f694e27
akashadhikari <akash_532998@yahoo.com>
2017-01-30 19:13:12 +0545

    img upload final

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -20,33 +20,35 @@
 
 	<div class="row">
 
-		{!! Form::model($post, ['route' => ['posts.update', $post->id], 'method' => 'PUT']) !!}
 
-		<div class="col-md-4">
-			<div class="well">
+		{!! Form::model($post, ['route' => ['posts.update', $post->id], 'method' => 'PUT', 'files' => true]) !!}
 
-				<dl class="dl-horizontal">
-					<dt>Augmentified at:</dt>
-					<dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
-				</dl>
+			<div class="col-md-4">
+		
+				<div class="well">
 
-				<dl class="dl-horizontal">
-					<dt>Regmented at:</dt>
-					<dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
-				</dl>
-				<hr>
-				<div class="row">
-					<div class="col-sm-6">
-						{{ Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block')) }}
+					<dl class="dl-horizontal">
+						<dt>Augmentified at:</dt>
+						<dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+					</dl>
+
+					<dl class="dl-horizontal">
+						<dt>Regmented at:</dt>
+						<dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+					</dl>
+					<hr>
+					<div class="row">
+						<div class="col-sm-6">
+							{{ Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block')) }}
+						</div>
+
+						<div class="col-sm-6">
+							{{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
+						</div>
 					</div>
 
-					<div class="col-sm-6">
-						{{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
-					</div>
-				</div>
-
+			    </div>
 		    </div>
-	    </div>
 
 	     <div class="col-md-8">
 	     	{{ Form::label('title', 'Business:') }}
@@ -58,7 +60,10 @@
 	     	{{ Form::label('category_id', 'Business Type:', ['class' => 'form-spacing-top']) }}
 	     	{{ Form::select('category_id', $categories, null, ['class' => 'form-control']) }}
 
-	     	{{ Form::label('title', 'Description:', ['class' => 'form-spacing-top']) }}
+	     	{{ Form::label('featured_image','Update Business Image', ['class' => 'form-spacing-top']) }} 
+			{{ Form::file('featured_image') }}
+
+	     	{{ Form::label('body', 'Description:', ['class' => 'form-spacing-top']) }}
 	     	{{ Form::textarea('body', null, ["class" => 'form-control']) }}
 	    	 
 	     </div> 
