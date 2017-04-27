6df732c8fecb1a15cb1b5bb6db6a55d4018df24a
Appollocyzt <appollocyzt.waynne@gmail.com>
2017-04-27 23:15:11 +0545

    adding api

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,7 +8,9 @@ use Session;
 
 class PagesController extends Controller {
 
-	public function getIndex() {
+	public function getIndex() 
+	{
+		
 		$posts = Post::orderBy('created_at', 'desc')->limit(4)->get();
 		return view('pages.welcome')->withPosts($posts);
 	}
