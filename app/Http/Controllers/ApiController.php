14d4a8b88ad8fdcc06b53e6b3318535498359aee
pandesantos <spsayal6@gmail.com>
2017-04-28 13:30:12 +0545

    api for all posts and categorywise matching

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 use App\Post;
+use App\Category;
+use App\Tag;
+use App\Comment;
+
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
-{
+{	
+	
+
     public function getAllPosts()
     {
-    	$post = Post::all();
-    	return $post->toJson();
-    }
-    public function getPostCategoryWise()
-    {
+    	// select all post with thier category
+    	$posts = Post::with('Category')->limit(20)->get();
 
+    	return $posts->toJson();
+    }
+    public function getPostCategoryWise($category)
+    {
+    	// only take those posts matching passed category
+    	$posts = Post::with('Category')->whereHas('Category', function($query) use ($category) {
+			 			$query->where('name', $category);
+						})->get();
+
+        return $posts->toJson();
     }
 }
