668860f14ce9d230a77f8e0edcdc60129dcaaee2
pandesantos <spsayal6@gmail.com>
2017-04-29 15:03:11 +0000

    navigation cleanup

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -32,7 +32,7 @@
             @if(Auth::check())
 
             <li class="{{ Request::is('categories') ? "active" : "" }}"><a href="{{ route('categories.index') }}">Categories</a></li>
-
+<!-- 
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Actions <span class="caret"></span></a>
               <ul class="dropdown-menu">
@@ -44,14 +44,15 @@
                 <li role="separator" class="divider"></li>
                 <li><a href="#">One more separated link</a></li>
               </ul>
-            </li>
+            </li> -->
+            
 
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hi {{ Auth::user()->name }} <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="{{ route('augments.index') }}">Augments</a></li>
-                <li><a href="{{ route('posts.index') }}">Posts</a></li>
-                <li><a href="{{ route('categories.index') }}">Categories</a></li>
+                <li><a href="{{ route('user.profile', ['userid' => Auth::user()->id] )}}">Your profile</a></li>
+                <li role="separator" class="divider"></li>
+                
                 <li><a href="{{ route('tags.index') }}">Tags</a></li>
                 <li role="separator" class="divider"></li>
                 <li><a href="{{ route('logout') }}">Log out</a></li>
