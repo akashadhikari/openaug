d469c96bd2d9ec6960cf8f03e905fd4e95e5cd5d
pandesantos <spsayal6@gmail.com>
2017-05-02 04:34:31 +0000

    added local bootstrap.css

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -9,7 +9,8 @@
     <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+    {{ Html::style('css/bootstrap.min.css')}}
     {{ Html::style('css/styles.css') }}
     {{ Html::style('css/jquery-ui.css')}}
 
