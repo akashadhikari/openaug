d27ee57bcda8555265aab5e407ddfe4727a904f9
akashadhikari <akash_532998@yahoo.com>
2017-07-27 13:16:04 +0545

    comments on posts view too

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -78,7 +78,45 @@
 
 	     </div>
 
-	     
+	     <div class="container">
+
+	     	<div class="row">
+				<div class="comment-form" class="col-md-8 col-md-offset-2" style="margin-top: 50px;">
+
+				{{ Form::open(['route' => ['comments.store', $post->id], 'method' => 'POST']) }}
+				<h3>Been Here? Add A Review</h3>
+
+	 				<div class="row">
+
+	 					<div class="col-md-6">
+	 						{{ Form::label('name','Your Name') }}
+	 						{{ Form::text('name', null, ['class' => 'form-control']) }}
+	 					</div>
+
+	 					<div class="col-md-6">
+	 						{{ Form::label('email','Your Email') }}
+	 						{{ Form::text('email', null, ['class' => 'form-control']) }}
+	 					</div>
+
+	 					<div class="col-md-12">
+	 						{{ Form::label('comment','Review The Augment') }}
+	 						{{ Form::textarea('comment', null, ['class' => 'form-control', 'rows'=> '5']) }}
+
+	 						{{ Form::submit('Submit Review', ['class' => 'pull-right btn btn-success', 'style' =>'margin-top: 15px;']) }}
+	 					</div>
+
+ 				</div>
+
+
+
+
+            {{ Form::close() }}
+
+		</div>
+	</div>
+	     	
+
+	     </div>
 
 
 	</div>
